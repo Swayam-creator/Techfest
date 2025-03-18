96572f446bfc2eb25eda400ee47a28252724a98f
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-17 22:44:08 -0700

    react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router'; // Correct import
-import Navbar from './Components/Navbar'; // Assuming Navbar is in a Components folder
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
+import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Event from './Pages/Event';
 import Speakers from './Pages/Speakers';
 import Cart from './Pages/Cart';
-import ChatBot from './Pages/ChatBot'
+import ChatBot from './Pages/ChatBot';
 import Cursor from './Components/Cursor';
 
 function App() {
   return (
     <div className='bg-black w-full h-screen'>
-    <Router>
-    
-      {/* Navbar is outside of Routes so it will be visible across all pages */}
-      <Cursor/>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/events' element={<Event />} />
-        <Route path='/speakers' element={<Speakers />} />
-        <Route path='/events-registred' element={<Cart />} />
-        <Route path='/chat-bot' element={<ChatBot />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Cursor />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/events' element={<Event />} />
+          <Route path='/speakers' element={<Speakers />} />
+          <Route path='/events-registred' element={<Cart />} />
+          <Route path='/chat-bot' element={<ChatBot />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
